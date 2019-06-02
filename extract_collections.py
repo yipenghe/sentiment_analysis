22bbdd27581d1cbc1c736dba40ce7dee9968f75a
yipenghe <yipenghe@umich.edu>
2019-06-02 16:22:56 -0700

    pros cons sentence collection

--- extract_collections.py
+++ extract_collections.py
@@ -3,5 +3,5 @@ from utils.dataLoaders import load_glassdoor
 X, y = load_glassdoor("collections", "train", 5, "cons", "sa")
 print(len(X))
 with open("cons_from_collection", "w") as f:
-    for text in X:
-        f.write(text+"\n")
+   for text in X:
+       f.write(text+"\n")
